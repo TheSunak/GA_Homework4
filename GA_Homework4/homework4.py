295ad1229b9080f296a36988f26169a148cf9c47
Vishal Sunak <ShowMeTheV-Money@Vishals-MacBook-Pro-3.local>
2013-10-06 19:12:59 -0400

    Working with split words for 'fuck'

--- GA_Homework4/homework4.py
+++ GA_Homework4/homework4.py
@@ -1,12 +1,76 @@
 import os
 import csv
-os.path
+import collections
+from collections import Counter
 
-os.listdir('/home/gregory/Dropbox/GA/homework4')
 
-train_data = csv.reader(open("train.csv", "rb"))
+#os.path
 
-for row in train_data:
-    print row
+#train_data = csv.reader(open("train.csv", "rb"))
 
+#for row in train_data:
+ #   print row
+
+
+#Create an array of the words of interest
+
+#search_words = [
+# 'fuck',
+# 'moron',
+# 'retard',
+# 'loser',
+# 'stupid',
+# 'sick',
+# 'dick',
+# 'ass',
+# 'nigga',
+# 'nigger',
+# 'pussy',
+# 'bitch',
+# 'faggot',
+# 'idiot',
+# 'crap',
+#]
+
+# test_line = "Hey fuck fuck fuck head"
+
+# word = test_line.split()
+
+# print word
+
+# cnt = Counter(word)
+
+# print cnt["fuck"]
+
+#Code from Stackoverflow:
+# grades = collections.Counter()
+# with open('train.csv') as input_file:
+#     for row in csv.reader(input_file, delimiter=','):
+#         grades[row[2]] += 1
+
+# print 'Number of A grades: %s' % grades['A']
+# print grades.most_common()
+
+grades = collections.Counter()
+with open('train2.csv') as input_file:
+        for row in csv.reader(input_file, delimiter=','):
+            print row[2]
+            split_words = row[2].split()
+            print split_words
+            cnt = Counter(split_words)
+
+            print cnt
+
+
+            #print cnt["fuck"]
+
+
+
+
+
+#build a For loop:
+
+#Reads in each line of the train file, determines if any of the words on the search words array is there
+
+#If it does, store the Row number into a results array... row number and Insults (1 or 0)
 
