6ab157b10f7ca78bb6bdbdd20d5aa083f4347145
ghovagim <ghovagim@gmail.com>
2013-10-06 18:24:34 -0400

    updated with current code

--- GA_Homework4/homework4.py
+++ GA_Homework4/homework4.py
@@ -1,1 +1,12 @@
-GREG!!!!!
+import os
+import csv
+os.path
+
+os.listdir('/home/gregory/Dropbox/GA/homework4')
+
+train_data = csv.reader(open("train.csv", "rb"))
+
+for row in train_data:
+    print row
+
+
